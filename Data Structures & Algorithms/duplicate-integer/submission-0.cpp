5a6a818c9a7f77678ee0f40f9d857a38767c137d
THUMU SAI <23U11A0442@gmail.com>
2026-09-04 01:01:27 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for(int x : nums){
+            if(seen.count(x)){
+                return true;
+            }
+
+            seen.insert(x);
+        }
+
+        return false;
+
+
+        
+    }
+};
